9d02ba6cf4c11451a073c56635d244151a75ebb8
Stephen Smoogen <smooge@redhat.com>
2020-02-04 19:38:13 +0000

    add proxy30 to config files

--- roles/base/files/syncHttpLogs.sh
+++ roles/base/files/syncHttpLogs.sh
@@ -41,6 +41,7 @@ syncHttpLogs proxy11.vpn.fedoraproject.org
 syncHttpLogs proxy12.vpn.fedoraproject.org
 syncHttpLogs proxy13.vpn.fedoraproject.org
 syncHttpLogs proxy14.vpn.fedoraproject.org
+syncHttpLogs proxy30.vpn.fedoraproject.org
 syncHttpLogs proxy101.phx2.fedoraproject.org
 syncHttpLogs proxy110.phx2.fedoraproject.org
 syncHttpLogs proxy01.stg.phx2.fedoraproject.org
