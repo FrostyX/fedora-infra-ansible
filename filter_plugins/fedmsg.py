0d3460f03a90fe61b3fa79709497cc439963a141
Adam Williamson <awilliam@redhat.com>
2020-06-11 12:19:34 -0700

    Drop fedmsg_qa_network concept
    I don't think this is needed any more. openQA publishes natively
    to fedora-messaging these days, and these groups never have done
    anything there. ResultsDB still publishes to fedmsg, it seems,
    but the new instances were not added to these groups and I don't
    think we need/want to do that, we should just make sure they
    have the necessary access via other more widely-used groups if
    anything. (It seems like the restrictions these groups used to
    affect have been - possibly temporarily - removed entirely at
    present, but we can figure out any necessary changes there if the
    restrictions are restored).
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- filter_plugins/fedmsg.py
+++ filter_plugins/fedmsg.py
@@ -9,7 +9,7 @@ def invert_fedmsg_policy(groups, vars, env):
     """
 
     if env == 'staging':
-        hosts = groups['all'] + groups['staging'] + groups['fedmsg_qa_network_stg']
+        hosts = groups['all'] + groups['staging']
     else:
         hosts = [h for h in groups['all'] if h not in groups['staging']]
 
