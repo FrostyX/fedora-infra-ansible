30d704f486a341e199e62b0efa905ef1117a9593
Francois Andrieu <naolwen@gmail.com>
2020-07-02 18:30:12 +0200

    nagios_client: make check_raid python 2/3 compatible
    Signed-off-by: Francois Andrieu <naolwen@gmail.com>

--- roles/nagios_client/files/scripts/check_raid.py
+++ roles/nagios_client/files/scripts/check_raid.py
@@ -1,16 +1,15 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 #
 # very simple python script to parse out /proc/mdstat
 # and give results for nagios to monitor
 #
 
 import sys
-import string
 
 devices = []
 
 try:
-    mdstat = string.split(open('/proc/mdstat').read(), '\n')
+    mdstat = open('/proc/mdstat').read().split('\n')
 except IOError:
     # seems we have no software raid on this machines
     sys.exit(0)
@@ -19,12 +18,12 @@ error = ""
 i = 0
 for line in mdstat:
     if line[0:2] == 'md':
-        device = string.split(line)[0]
+        device = line.split()[0]
         devices.append(device)
-        status = string.split(mdstat[i+1])[-1]
-        if string.count(status, "_"):
+        status = mdstat[i+1].split()[-1]
+        if status.count("_"):
             # see if we can figure out what's going on
-            err = string.split(mdstat[i+2])
+            err = mdstat[i+2].split()
             msg = "device=%s status=%s" % (device, status)
             if len(err) > 0:
                 msg = msg + " rebuild=%s" % err[0]
@@ -36,10 +35,9 @@ for line in mdstat:
     i = i + 1
 
 if not error:
-    print "DEVICES %s OK" % " ".join(devices)
+    print("DEVICES %s OK" % " ".join(devices))
     sys.exit(0)
 
 else:
-    print error
+    print(error)
     sys.exit(2)
-
