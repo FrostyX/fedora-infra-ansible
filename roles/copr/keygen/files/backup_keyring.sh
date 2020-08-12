5e38e064d0fc0d6ad4e2253fc1d4f27b6c99375c
Pavel Raiskup <praiskup@redhat.com>
2020-08-12 18:03:41 +0200

    copr-keygen: use correct --recipient for gpg-encrypted backups

--- roles/copr/keygen/files/backup_keyring.sh
+++ roles/copr/keygen/files/backup_keyring.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # used as root
-# root gpg keychain should have PUBLIC key with `user email` admin@fedoraproject.org
+# root gpg keychain should have PUBLIC key with `user name` copr-keygen-backup-key
+# (per https://pagure.io/fedora-infrastructure/issue/8904)
 
 PATH_TO_KEYRING_DIR="/var/lib/copr-keygen"
 BACKUP_DIR=/backup
@@ -9,7 +10,7 @@ OUTPUT_FILE="$BACKUP_DIR/copr_keygen_keyring_$(date -I).tar.gz.gpg"
 
 tar --exclude="*agent*" -czPf - "$PATH_TO_KEYRING_DIR" \
     | gpg2 --output "$OUTPUT_FILE".tmp --encrypt \
-           --recipient admin@fedoraproject.org --always-trust \
+           --recipient copr-keygen-backup-key --always-trust \
 && mv "$OUTPUT_FILE.tmp" "$OUTPUT_FILE"
 
 # shell pattern matching provides sorted output
