5f114a7c987e9c4df27baffac28b82ec0906e669
Mark O'Brien <markobri@redhat.com>
2020-08-24 17:21:00 +0100

    [proxies] setup for proxy35/36

--- roles/base/files/syncHttpLogs.sh
+++ roles/base/files/syncHttpLogs.sh
@@ -52,6 +52,8 @@ syncHttpLogs proxy31.vpn.fedoraproject.org
 syncHttpLogs proxy32.vpn.fedoraproject.org
 syncHttpLogs proxy33.vpn.fedoraproject.org
 syncHttpLogs proxy34.vpn.fedoraproject.org
+syncHttpLogs proxy35.vpn.fedoraproject.org
+syncHttpLogs proxy36.vpn.fedoraproject.org
 syncHttpLogs proxy101.iad2.fedoraproject.org
 syncHttpLogs proxy110.iad2.fedoraproject.org
 # syncHttpLogs proxy01.stg.iad2.fedoraproject.org
