b2cdf5dc62717a618e63a82c8cce3bfb5e604a4d
Aurélien Bompard <aurelien@bompard.org>
2020-08-31 22:58:51 +0200

    Now that IPA is the reference, allow users to change their own attributes
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/ipa/server/files/configure-ipa.sh
+++ roles/ipa/server/files/configure-ipa.sh
@@ -8,15 +8,5 @@ trap cleanup EXIT
 
 echo $ADMIN_PASSWORD | kinit admin
 
-# Disallow all users to change their own settings
-ipa selfservice-find | grep "Self-service name:" | sed -e "s/  Self-service name: //" | \
-while read line
-do
-    echo "Removing $line"
-    ipa selfservice-del "$line"
-done
-
 # Disable default permissions so we don't break our privacy policy
 ipa permission-mod "System: Read User Addressbook Attributes" --bindtype=permission
-
-# TODO: Add custom permissions to grant specific access to user attributes
