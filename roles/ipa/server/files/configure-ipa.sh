944431bf595c303ad58d942696dd11d95e0ab424
Aurélien Bompard <aurelien@bompard.org>
2020-09-01 00:11:30 +0200

    IPA: Allow users to read their own data
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/ipa/server/files/configure-ipa.sh
+++ roles/ipa/server/files/configure-ipa.sh
@@ -10,3 +10,9 @@ echo $ADMIN_PASSWORD | kinit admin
 
 # Disable default permissions so we don't break our privacy policy
 ipa permission-mod "System: Read User Addressbook Attributes" --bindtype=permission
+
+# Allow users to read their own data (needed because of the previous line)
+ipa selfservice-find "Users can read their own addressbook attributes" --pkey-only || \
+ipa selfservice-add "Users can read their own addressbook attributes" \
+    --permissions read \
+    --attrs mail --attrs userCertificate --attrs ipaCertmapData
