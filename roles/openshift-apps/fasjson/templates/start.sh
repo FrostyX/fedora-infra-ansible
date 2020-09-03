ca7ddc8b0900d0b75c640009699f882b65944b4d
Aurélien Bompard <aurelien@bompard.org>
2020-09-03 23:09:59 +0200

    FASJSON: Move the ccache config in apache's conf
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/openshift-apps/fasjson/templates/start.sh
+++ roles/openshift-apps/fasjson/templates/start.sh
@@ -4,5 +4,4 @@ mkdir /httpdir/run/ /httpdir/run/ccaches/
 ln -s /etc/httpd/modules /httpdir/modules
 truncate --size=0 /httpdir/access.log /httpdir/error.log
 tail -qf /httpdir/access.log /httpdir/error.log &
-export KRB5CCNAME=/httpdir/httpd.ccache
 exec httpd -f /etc/fasjson/httpd.conf -DFOREGROUND -DNO_DETACH
