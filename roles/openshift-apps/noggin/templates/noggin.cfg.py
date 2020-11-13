ba812732c2a12502ea603a3af0d6f87febfe9428
Aurélien Bompard <aurelien@bompard.org>
2020-11-13 09:20:35 +0100

    Disable registration in Noggin, for now
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/openshift-apps/noggin/templates/noggin.cfg.py
+++ roles/openshift-apps/noggin/templates/noggin.cfg.py
@@ -40,3 +40,6 @@ SECRET_KEY = from_file('/etc/noggin-secrets/session').encode('utf-8')
 
 # Spam checking
 # BASSET_URL = None
+
+# Disable registration until the account import is complete
+REGISTRATION_OPEN = False
